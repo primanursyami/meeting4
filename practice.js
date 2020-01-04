d9c2989e5cd5a7f6ec2fe1a83d78f80d4eaee0a7
Prima Nursyami <prima.nursyami@gmail.com>
2020-01-04 15:40:27 +0700

    meeting4

---
+++ practice.js
@@ -0,0 +1,58 @@
+function luasSegitiga (panjang, lebar){
+    let result = (panjang*lebar/2)
+    return result
+}
+
+let segitigaSatu = luasSegitiga (2, 4)
+console.log (segitigaSatu)
+
+let segitigaDua = luasSegitiga (5, 6)
+console.log (segitigaDua)
+
+function luasLingkaran (jari){
+    let result2 = (3.14*jari**2)
+    return result2
+}
+
+let lingkaranSatu = luasLingkaran (7)
+console.log (lingkaranSatu)
+
+function luasLingkaran2 (diameter){
+    let result3 = (3.14*(diameter/2)**2)
+    return result3
+}
+
+let lingkaranTiga = luasLingkaran2 (14)
+console.log (lingkaranTiga)
+
+// function kelulusan (nilai){
+//     let result4;
+//     if (nilai>80){result4='lulus'}{
+//         else {result4='tidak lulus'}
+//     }
+    
+//     return result4
+// }
+// let budi = kelulusan (70)
+// console.log ('budi', budi)
+
+function printNumber (number){
+    let result = ''
+    for (let i=number; i>0; i--)
+    return result += 
+    
+let numberOne = printNumber(10)
+console.log(numberOne) 
+
+let me= [
+    {Name: 'prima',
+    Age: 32,
+    Gender: 'male',
+    Phone: 082122686028
+    }, {Name: 'nursyami',
+Age: 33,
+Hobby: ['read', 'run']}
+]
+
+console.log(me)
+
